da0aefaabc072dfa16477abbaa5ffb120febbdfe
Yanis Metari <yanismetari@MacBook-Air-de-Yanis.local>
2026-02-02 21:46:39 +0100

    adding user and pin part, and fixing the rest api (now working to create a pin)

--- map_bb/backend_map/index.js
+++ map_bb/backend_map/index.js
@@ -2,20 +2,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv")
+const pinRoute = require("./routes/pins")
 
 // App initialization
 const app = express();
 
 dotenv.config();
-
+app.use(express.json())
 
 //using dotenv for security reasons () hide the database connection string)
 mongoose.connect(process.env.Mongo_Url)
+
 .then(() => {
    console.log("MongoDB connected!")
 })
 .catch((err) => console.error("Error with MongoDB connection attempt:", err));
 
+app.use("/api/pins", pinRoute);   
+
 app.listen(3000, ()=>{
     console.log("Backend server is running !")
 })
+
