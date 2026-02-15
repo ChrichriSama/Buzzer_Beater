4d44c6de3d0ae8a80dab70c4776c0e6365ea8b71
Yanis Metari <yanismetari@MacBook-Air-de-Yanis.local>
2026-02-15 15:33:51 +0100

    feat:Connecting the front end with the backend for the map

--- map_bb/frontend_map/src/App.jsx
+++ map_bb/frontend_map/src/App.jsx
@@ -1,30 +1,74 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Map from 'react-map-gl/mapbox';
 import {Marker} from 'react-map-gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import RoomIcon from '@mui/icons-material/Room';
-
+import  {Popup} from 'react-map-gl/mapbox';
+import StarIcon from '@mui/icons-material/Star';
+import './app.css';
+import axios from "axios"
+import {format} from "timeago.js"
 
   function App() {
+    const[pins, setPins]= useState([])
+    useEffect(() => {
+      const getPins = async ()=>{
+        try {
+          const res = await axios.get("/api/pins");
+          setPins(res.data)
+          console.log(res.data)
 
+        }catch(err){
+          console.error("Issue with getting all pins (getPins function)",err)
+        }
+      };
+      getPins()
+    },[])
     return (
-
         <div className="App">
+
           <Map
             mapboxAccessToken={import.meta.env.VITE_MAPBOX}
             initialViewState={{
               longitude: 2.294481,
               latitude: 48.858370,
-              zoom: 10
+              zoom: 16
             }}
+            mapStyle="mapbox://styles/loey25/cmlif4xol002u01sk7h2xbcpl"
             style={{width: "100vw", height: "100vh"}}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
             >
-                <Marker longitude={2.294481} latitude={48.858370} anchor="bottom" >
-                <div>ici</div>
-                <RoomIcon style={{color: "red", fontSize: visualViewport.zoom }}/>
-                </Marker>
-          </Map>;
+              {pins.map((p) => (
+                <>
+                  <Marker longitude={p.long} latitude={p.lat} anchor="bottom" >
+                  <RoomIcon style={{color: "red", fontSize: visualViewport.zoom }}/>
+                  </Marker>
+
+                  <Popup longitude={p.long} latitude={p.lat}
+                    anchor="top"
+                    onClose={() => setShowPopup(false)}>
+                    <div className='card'>
+                      <label>Place</label>
+                      <h4 className='place'>{p.title}</h4>
+                      <label>address</label>
+                      <p>{p.address}</p>
+                      <label>Review </label>
+                      <p className='desc'>{p.desc}</p>
+                      <label>Rating</label>
+                      <div className="stars">
+                        <StarIcon className="star"/>
+                        <StarIcon className="star"/>
+                        <StarIcon className="star"/>
+                        <StarIcon className="star"/>
+                        <StarIcon className="star"/>
+                      </div>
+                      <span className="username">Created by <b>{p.username}</b></span>
+                      <br></br>
+                      <span className="date"> {format(p.createdAt)}</span>
+                    </div>
+                  </Popup>
+                </>
+              ))}
+          </Map>
 
         </div>
 
